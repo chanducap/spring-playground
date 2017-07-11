a1e7f4ac5bf63449b834dd6db7225baaba53a069
chandra <chandra-shekhar-rao.gurram@capgemini.com>
2017-07-11 12:09:29 -0400

    Calculate with Querystrings

--- src/test/java/com/gal/SpringPlaygroundApplicationTests.java
+++ src/test/java/com/gal/SpringPlaygroundApplicationTests.java
@@ -1,41 +1,5 @@
 package com.gal;
 
-import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
-
-import com.gal.controller.MathController;
-import com.gal.service.MathService;
-
-@RunWith(SpringRunner.class)
-@ContextConfiguration
-@WebMvcTest(MathController.class)
-
 public class SpringPlaygroundApplicationTests {
-
-	@Autowired
-	private WebApplicationContext ctx;
-
-	@MockBean
-	private MathService mathService;
-
-	@Autowired
-	private MockMvc mockMvc;
-
-		
 
 }
