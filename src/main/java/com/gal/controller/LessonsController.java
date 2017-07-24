064e73146bc8fc0028ed4646e2b83d5356151dff
chandra <chandra-shekhar-rao.gurram@capgemini.com>
2017-07-24 13:41:12 -0400

    spring lessons:crud endpoints

--- src/main/java/com/gal/controller/LessonsController.java
+++ src/main/java/com/gal/controller/LessonsController.java
@@ -1,7 +1,9 @@
 package com.gal.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,14 +22,24 @@ public class LessonsController {
         this.repository = repository;
     }
 
-    @GetMapping("")
+    /*@GetMapping("")
     public Iterable<Lesson> all() {
         return this.repository.findAll();
+    }*/
+    
+    @GetMapping("/{user}")
+    public Lesson all(@PathVariable Long user) {
+        return this.repository.findOne(user);
     }
 
     @PostMapping("")
     public Lesson create(@RequestBody Lesson lesson) {
         return this.repository.save(lesson);
+    }
+    
+    @DeleteMapping("/{DeleteUserId}")
+    public void DeleteUser(@PathVariable Long DeleteUserId) {
+         this.repository.delete(DeleteUserId);
     }
 
 }
